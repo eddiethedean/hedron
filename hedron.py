3071e89ac6cfeecdedd3e503e4144ed5aad7af0d
Odos Matthews <41598150+eddiethedean@users.noreply.github.com>
2019-12-05 14:16:44 -0600

    added colocation functions

--- hedron.py
+++ hedron.py
@@ -1,24 +1,46 @@
 import pandas as pd
+import cluster_functions as cl
 
 class Cluster(pd.DataFrame):
     """Holds a pandas DataFrame with coordinate data"""
-    def __init__(self, df, lat_header, lon_header, date_time_header, id_header=''):
+    def __init__(self, df, lat_header, lon_header, date_time_header, id_header):
         pd.DataFrame.__init__(self, df)
         self.lat_header = lat_header
         self.lon_header = lon_header
         self.id_header = id_header
         self.date_time_header = date_time_header
-        # Add day column
-        df['day'] = df[date_time_header].dt.date
+        self.day_header = 'day'
+        if len(df) == 0: return
 
+        # Try to convert columns to correct data types
+        self[lat_header] = self[lat_header].astype(float)
+        self[lon_header] = self[lon_header].astype(float)
+        self[date_time_header] = pd.to_datetime(df[date_time_header])
+        self[id_header] = self[id_header].astype(str)
+        # Add day column
+        df[self.day_header] = df[date_time_header].dt.date
 
     def make_clusters(self, digits):
-        return SuperCluster(cluster_coords(self, self.lat_header, self.lon_header, digits))
+        if len(self)==0: return SuperCluster(dict())
+        return convert_dict_to_super(self, cl.cluster_coords(self, self.lat_header, self.lon_header, digits))
+
+    def colocation_clusters(self, digits):
+        if len(self)==0: return SuperCluster(dict())
+        return convert_dict_to_super(self, cl.colocation_cluster_coords(self, self.lat_header, self.lon_header, self.id_header, digits))
+
+    def day_colocation_cluster(self):
+        if len(self)==0: return self
+        return Cluster(cl.day_colocations(self, self.day_header, self.id_header), self.lat_header, self.lon_header, self.date_time_header, self.id_header)
+
+    def day_colocation_clusters(self):
+        if len(self)==0: return SuperCluster(dict())
+        return convert_dict_to_super(self, cl.day_colocations(self, self.day_header, self.id_header, merge=False))
 
 
 class SuperCluster(dict):
     """Holds multiple Cluster Objects"""
     def __init__(self, data):
+        """data if a dictionary of Cluster objects"""
         dict.__init__(self, data)
 
     def clusters(self):
@@ -27,6 +49,29 @@ class SuperCluster(dict):
     def names(self):
         return self.keys()
 
+    def colocation_clusters(self):
+        if len(self)==0: return self
+        return SuperCluster({key:cluster for key, cluster in self.items() if len(cluster[cluster.id_header].unique())>1})
+
+    def merge(self):
+        # TODO: Combine each cluster into one cluster
+        pass
+
+    def day_colocation_clusters(self):
+        if len(self)==0: return self
+        day_clusters = dict()
+        for key, cluster in self.items():
+            c = cluster.day_colocation_cluster()
+            if len(c) > 0:
+                day_clusters[key] = c
+        return SuperCluster(day_clusters)
+
+
+    # TODO: to_xlsx method, stores each cluster in a tab. saves in xlsx file
+
+def convert_dict_to_super(cluster, d):
+    if len(d)==0: return SuperCluster(dict())
+    return SuperCluster({key:Cluster(df, cluster.lat_header, cluster.lon_header, cluster.date_time_header, cluster.id_header) for key, df in d.items()})
 
 
 def main():
@@ -48,73 +93,11 @@ def main():
     print(len(clusters))
 
     # Filter clusters to colocation clusters
-    #colocations = f
+    colocations = clusters.colocation_clusters()
+    print(len(colocations))
 
-    #day_co = f
-
-
-
-def day_colocations_clusters(clusters, day_header, id_header):
-    """Check each cluster for ids on same day"""
-    # future summary function output {key:day, ids counts}
-    # this function output all the metadata for each day colocation point
-    out = dict()
-    for key,df in clusters.items():
-        day_co = day_colocations(df, day_header, id_header)
-        if len(day_co) > 0:
-            out[key] = day_co
-    return out
-
-
-def day_colocations(cluster, day_header, id_header):
-    cluster = cluster.copy()
-    day_clusters = cluster.groupby(day_header)
-    colocated = {key:df for key, df in day_clusters if len(df[id_header].unique())>1}
-    if len(colocated) == 0:
-        return pd.DataFrame()
-    # Add back date to each df
-    for key, df in colocated.items():
-        df[day_header] = [key for _ in range(len(df))]
-    # Combine DataFrames
-    return pd.concat(colocated.values(), axis=0)
-
-
-def cluster_coords(df, lat_header, lon_header, digits):
-    df = df.copy()
-    # Make lat,lon hash column
-    df['hash'] = [hash_latlon(lat, lon, digits) for lat, lon in zip(df[lat_header], df[lon_header])]
-    # Make dict with hash:cluster, clusters need more than 1 point to count as a cluster
-    return {key:cluster_df for key, cluster_df in df.groupby('hash') if len(cluster_df) > 1}
-
-
-def colocation_clusters(clusters, id_header):
-    """Return only clusters with more than one id"""
-    return {key:df for key, df in clusters.items() if len(df[id_header].unique()) > 1}
-
-
-def colocation_cluster_coords(df, lat_header, lon_header, id_header, digits):
-    df = df.copy()
-    # Make lat,lon hash column
-    df['hash'] = [hash_latlon(lat, lon, digits) for lat, lon in zip(df[lat_header], df[lon_header])]
-    # Make dict with hash:colocation cluster, clusters need more than 1 id to be a colocation cluster
-    return {key:cluster_df for key, cluster_df in df.groupby('hash') if len(cluster_df[id_header].unique()) > 1}
-
-
-def hash_latlon(lat, lon, i):
-    """Get hash from lat/lon | 14.60775948, -90.65505981, 3 --> '14.607,-90.655"""
-    return cut_decimal(lat, i) + ',' + cut_decimal(lon, i)
-
-
-def cut_decimal(f, i):
-    """Converts float f to a string with i digits after decimal place"""
-    num = str(f)
-    period = num.find(".") + 1
-    if period == 0:
-        return num & "." + (i * "0")
-    decimals = len(num) - period
-    if decimals < i:
-        return num + ((i - decimals) * "0")
-    return num[0:period + i]
+    day_co = colocations.day_colocation_clusters()
+    print(len(day_co))
 
 
 def fix_df_dates(df, date_header):
